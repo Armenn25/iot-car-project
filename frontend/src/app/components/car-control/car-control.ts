837d8cbd84575a0b5a927c383664159cee650a48
Armenn25 <168114760+Armenn25@users.noreply.github.com>
2025-10-19 12:57:12 +0200

    feat(setup): add Angular Material, custom theme, and base configuration

---
+++ frontend/src/app/components/car-control/car-control.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-control',
+  imports: [],
+  templateUrl: './car-control.html',
+  styleUrl: './car-control.css'
+})
+export class CarControl {
+
+}
