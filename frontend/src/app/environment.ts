e82e3bfc13b2f915f8c17c28711fc49123bdd929
Armenn25 <168114760+Armenn25@users.noreply.github.com>
2025-10-19 21:11:58 +0200

    hotfix:2

--- frontend/src/app/environment.ts
+++ frontend/src/app/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://20.199.88.208',          // <-- ovo stavi na IP tvoje VM!
-  signalRHub: 'http://20.199.88.208/carhub' // <-- i ovdje, koristi isti IP i ime SignalR huba
+  apiUrl: 'https://iotcarbe.xyz',       // tvoja nova domena sa HTTPS
+  signalRHub: 'https://iotcarbe.xyz/carhub' // signalR hub na novoj domeni
 };
